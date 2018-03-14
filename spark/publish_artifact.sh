ea37f044f247213e843fc63b8c4eb991f5b57cc6
Brent Hamilton <brent@arthurkn.net>
2018-03-14 14:37:34 -0700

    SCALA_MAJOR_VERSION

--- spark/publish_artifact.sh
+++ spark/publish_artifact.sh
@@ -11,5 +11,5 @@ sbt package
 
 gcloud auth activate-service-account --key-file /deploy/gcskey.json
 
-gsutil cp target/scala-${SCALA_BASE_VERSION}/${DEFAULT_JAR} gs://uncoil-artifacts/${JOB_NAME}/${JOB_NAME}:${ARTIFACT_TAG}.jar
-gsutil cp target/scala-${SCALA_BASE_VERSION}/${DEFAULT_JAR} gs://uncoil-artifacts/${JOB_NAME}/${JOB_NAME}:latest.jar
+gsutil cp target/scala-${SCALA_MAJOR_VERSION}/${DEFAULT_JAR} gs://uncoil-artifacts/${JOB_NAME}/${JOB_NAME}:${ARTIFACT_TAG}.jar
+gsutil cp target/scala-${SCALA_MAJOR_VERSION}/${DEFAULT_JAR} gs://uncoil-artifacts/${JOB_NAME}/${JOB_NAME}:latest.jar
