932f439240742baf9a861b162a50870909662a2b
jakeorr <jake.a.orr@gmail.com>
2018-03-23 13:03:25 -0700

    Using dependency jars along side the main jar.

--- spark/publish_artifact.sh
+++ spark/publish_artifact.sh
@@ -9,7 +9,13 @@ ARTIFACT_TAG="$CI_COMMIT_ID"
 
 sbt package
 
+# compress the dependencies
+tar -czvf dependencies.tar.gz lib_managed/jars
+
 gcloud auth activate-service-account --key-file /deploy/gcskey.json
 
 gsutil cp target/scala-${SCALA_MAJOR_VERSION}/${DEFAULT_JAR} gs://uncoil-artifacts/${JOB_NAME}/${JOB_NAME}:${ARTIFACT_TAG}.jar
-gsutil cp target/scala-${SCALA_MAJOR_VERSION}/${DEFAULT_JAR} gs://uncoil-artifacts/${JOB_NAME}/${JOB_NAME}:latest.jar
+gsutil cp gs://uncoil-artifacts/${JOB_NAME}/${JOB_NAME}:${ARTIFACT_TAG}.jar gs://uncoil-artifacts/${JOB_NAME}/${JOB_NAME}:latest.jar
+
+gsutil cp dependencies.tar.gz gs://uncoil-artifacts/${JOB_NAME}/${JOB_NAME}_dependencies:${ARTIFACT_TAG}.tar.gz
+gsutil cp gs://uncoil-artifacts/${JOB_NAME}/${JOB_NAME}_dependencies:${ARTIFACT_TAG}.tar.gz gs://uncoil-artifacts/${JOB_NAME}/${JOB_NAME}_dependencies:latest.tar.gz
--- spark/pull_artifact.sh
+++ spark/pull_artifact.sh
@@ -9,3 +9,7 @@ BRANCH=$(cd /git; git rev-parse --abbrev-ref HEAD)
 HASH=$(cd /git; git rev-parse ${BRANCH})
 
 gsutil cp gs://uncoil-artifacts/${JOB_NAME}/${JOB_NAME}:${HASH}.jar /opt/spark/work-dir/${JOB_NAME}.jar
+gsutil cp gs://uncoil-artifacts/${JOB_NAME}/${JOB_NAME}_dependencies:${HASH}.tar.gz /opt/spark/work-dir/dependencies.tar.gz
+
+tar -xzvf /opt/spark/work-dir/dependencies.tar.gz -C /opt/spark/lib
+rm /opt/spark/work-dir/dependencies.tar.gz
