a9cfa51f906603b69eb068d249dc91e009169b02
dv-blk <dev.blakeandersen@gmail.com>
2018-08-29 11:12:57 -0700

    delete job on deploy

--- deploy_to_kubernetes_cron.sh
+++ deploy_to_kubernetes_cron.sh
@@ -8,6 +8,7 @@ source /deploy/kubernetes_deploy_base.sh
 JOB_NAME=$NAME
 
 /deploy/kubernetes_deploy_base.sh
+
 export PRIMARY_IMAGE="${PRIMARY_IMAGE}"
 
 for command in "${!commands[@]}"; do
@@ -23,11 +24,12 @@ for command in "${!commands[@]}"; do
   export SCHEDULE="${commands[$command]}"
   export NAME="${NAME}"
 
-
   echo COMMAND "$COMMAND"
   echo NAME "$NAME"
   echo SCHEDULE "$SCHEDULE"
   echo PRIMARY_IMAGE "$PRIMARY_IMAGE"
+
+  kubectl delete cronjob $NAME
 
   /deploy/templater.sh ${JOB_TEMPLATE} > ${JOB_FILE}
   kubectl apply --record -f ${JOB_FILE}
