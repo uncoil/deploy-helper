5f748cbbe2874e767b06650ac12756d632b1c274
jakeorr <jake.a.orr@gmail.com>
2019-08-27 09:11:39 -0700

    add debug output

--- deploy_to_kubernetes.sh
+++ deploy_to_kubernetes.sh
@@ -8,6 +8,9 @@ export PRIMARY_IMAGE="${PRIMARY_IMAGE}"
 echo "Upgrading to $PRIMARY_IMAGE"
 /deploy/templater.sh /deploy/kubernetes/deployment.template.yaml > deployment.yaml
 
+# TODO: testing
+cat deployment.yaml
+
 echo "Applying new deployment"
 kubectl apply --record -f deployment.yaml
 echo "Done"
